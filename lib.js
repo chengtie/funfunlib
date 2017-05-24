d4b5312487e38003ec2bc451b6da81ac1c3719a6
chengtie <chengtie@gmail.com>
2017-05-24 12:38:41 +0800

    testing

--- lib.js
+++ lib.js
@@ -62,8 +62,8 @@ function table2tree(table) {
     });
 
     if (tree === []) return {};
-    else { alert(JSON.stringify(tree)); if (tree[0].parent === null) return tree[0]; }
-    else return { "name": tree[0].parent, "parent": null, "children": tree }; // add "flare"
+    else { alert(JSON.stringify(tree)); if (tree[0].parent === null) return tree[0]; 
+    else return { "name": tree[0].parent, "parent": null, "children": tree }; // add "flare" }
 }
 
 // input: [["analytics", "flare", null], ["cluster", "analytics", null], ["AgglomerativeCluster", "cluster", 3938], ...
